bb6d0195a1612da6e81251b077866b593f09cb60
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2026-03-10 17:59:14 -0500

    tuesday update fix
    fixed it!

--- code.js
+++ code.js
@@ -1,28 +1,5 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-function helloWorld() {
-
-  alert("Hello, World!");
-=======
 function toSite(siteLocation) {
 
   window.location.href = siteLocation;
->>>>>>> Stashed changes
-=======
-function toSite(siteLocation) {
 
-  window.location.href = siteLocation;
->>>>>>> Stashed changes
-=======
-function toSite(siteLocation) {
-
-  window.location.href = siteLocation;
->>>>>>> Stashed changes
-=======
-function toSite(siteLocation) {
-
-  window.location.href = siteLocation;
->>>>>>> Stashed changes
 }
