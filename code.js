cfd7226705a686a3c6314e8c4e6aaa7a70716f50
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2026-03-15 16:13:06 -0500

    mobile margin works!
    yippee

--- code.js
+++ code.js
@@ -3,23 +3,23 @@ function toSite(siteLocation) {
   window.location.href = siteLocation;
 
 }
-console.log('running');
+
 function setMargin(writeLocation) {
 
-  console.log(writeLocation);
 
   writeLocation = document.getElementById(writeLocation);
-  writeLocation.classList.add('margin-on');
-
-  console.log(writeLocation);
+  writeLocation.classList.remove('margin-on');
+  writeLocation.classList.remove('margin-off');
 
   if (window.innerWidth <= window.innerHeight) {
+
     console.log('width <= height');
     writeLocation.classList.remove('margin-on');
     writeLocation.classList.add('margin-off');
+    
   } else {
     console.log('width > height');
+    writeLocation.classList.remove('margin-off');
+    writeLocation.classList.add('margin-on');
   }
 }
-
-setMargin('main');
