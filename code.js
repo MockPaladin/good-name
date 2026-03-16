199a69345a484600a00f344b801ef8f6090e31e9
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2026-03-15 23:00:34 -0500

    fixed very slight error
    "asdf" as a var name?

--- code.js
+++ code.js
@@ -12,17 +12,15 @@ function setMargin(writeLocation, asdf) {
   writeLocation.classList.remove('margin-off');
 
   if (window.innerWidth <= window.innerHeight) {
-
-    console.log('width <= height');
     writeLocation.classList.remove('margin-on');
     writeLocation.classList.add('margin-off');
     
   } else {
-    console.log('width > height');
     writeLocation.classList.remove('margin-off');
     writeLocation.classList.add('margin-on');
   }
-  asdf = document.getElementById(asdf)
-  asdf.classList.remove('loading')
-  asdf.classList.add('loaded')
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  setMargin("main");
+  });
