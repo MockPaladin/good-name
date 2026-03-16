7e3663f296aa68d0f3be9929e6172cb5f1ea32ef
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2026-03-16 17:10:41 -0500

    finished margins
    aw yeah

--- code.js
+++ code.js
@@ -7,20 +7,31 @@ function toSite(siteLocation) {
 function setMargin(writeLocation) {
 
 
-  writeLocation = document.getElementById(writeLocation);
-  writeLocation.classList.remove('margin-on');
-  writeLocation.classList.remove('margin-off');
+  writeLocation = document.querySelectorAll(writeLocation);
 
-  if (window.innerWidth <= window.innerHeight) {
-    writeLocation.classList.remove('margin-on');
-    writeLocation.classList.add('margin-off');
-    
-  } else {
-    writeLocation.classList.remove('margin-off');
-    writeLocation.classList.add('margin-on');
+  for (let i = 0; i < writeLocation.length; i++) {
+
+    if (window.innerWidth <= 850) {
+
+      writeLocation[i].style.marginLeft = "0%";
+      writeLocation[i].style.marginRight = "0%";
+    } else {
+
+      
+      writeLocation[i].style.marginLeft = "20%";
+      writeLocation[i].style.marginRight = "20%";
+    }
   }
+
+  
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   setMargin("main");
   });
+
+window.addEventListener('resize', function() {
+  setMargin("main");
+  });
+
+console.log("JavaScript has finished loading. Hello from random person!");
