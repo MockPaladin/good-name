5e41c84e2daba571c64708d05837963c7a2c357c
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2026-03-17 14:19:43 -0500

    Update code.js
    le spacing

--- code.js
+++ code.js
@@ -11,11 +11,9 @@ function setMargin(writeLocation) {
   for (let i = 0; i < writeLocation.length; i++) {
 
     if (window.innerWidth <= 850) {
-
       writeLocation[i].style.marginLeft = "0%";
       writeLocation[i].style.marginRight = "0%";
     } else {
-
       writeLocation[i].style.marginLeft = "20%";
       writeLocation[i].style.marginRight = "20%";
     }
