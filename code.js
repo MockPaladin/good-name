3735e2bd6049a5e6858f111e7720992c89dc7325
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2026-03-21 11:10:33 -0500

    small edit
    sidebar works!

--- code.js
+++ code.js
@@ -18,6 +18,24 @@ function sidebarVisibility() {
 
 document.addEventListener('DOMContentLoaded', function() {
   sidebarVisibility();
+
+  const p = document.getElementById("nav");
+  const sections = document.querySelectorAll("section[id]");
+
+  const fragment = document.createDocumentFragment();
+
+  sections.forEach(el => {
+
+    const a = document.createElement("a");
+    a.href = `#${el.id}`;
+
+    const heading = el.querySelector("h1, h2, h3");
+    a.textContent = heading ? heading.textContent : el.id;
+
+    fragment.appendChild(a);
+    });
+
+  p.appendChild(fragment);
   });
 
 window.addEventListener('resize', function() {
